0f764d4093fcb84f74f31bf2f2907b2d9f3168ee
Subash R <waytosubash@gmail.com>
2022-12-13 19:36:43 +0530

    Adding directory structure

--- lib/mt_line_eraser.rb
+++ lib/mt_line_eraser.rb
@@ -1,15 +1,24 @@
 class MtLineEraser
   class << self
-    def erase_in(file_name_with_path)
-      return "Invalid File path" unless File.exists?(file_name_with_path) 
-      original_file = File.open(file_name_with_path, 'r')
-      clone_file    = File.open(file_name_with_path + '.clone', 'w')
+    def erase_in(file_or_dir_path)
+      return "Invalid File path" unless File.exists?(file_or_dir_path)
+      files_to_be_processed = File.file?(file_or_dir_path) ? file_or_dir_path
+                                                           : Dir["#{file_or_dir_path}/**/*.*"]
+      files_to_be_processed.each do |_file|
+        erase_empty_lines_in_file(_file)
+        p "Erased all the empty lines from #{_file}"
+      end
+      return nil
+    end
+
+    def erase_empty_lines_in_file(file_path)
+      original_file = File.open(file_path, 'r')
+      clone_file    = File.open(file_path + '.clone', 'w')
       original_file.readlines.each do |line|
         clone_file.write(line) if line.gsub(/^[\s|\n]*/, '') != ''
       end
       [original_file, clone_file].map{|_file| _file.close}
       File.rename(clone_file, original_file)
-      "Erased all the empty lines from #{file_name_with_path}"
     end
   end
 end
